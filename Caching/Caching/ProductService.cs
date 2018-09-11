6a98e0a9242bf52820d3068ba92d22b3dfff4323
Bhargavi Deshpande <bdeshpande@tavisca.com>
2018-09-11 19:36:42 +0530

    caching projrct added

---
+++ Caching/Caching/ProductService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caching
+{
+    class ProductService : IProductService
+    {
+        public Product GetProductById(int id)
+        {
+            IRepository productRepo = new SqlRepository();
+            Console.WriteLine("Data From Database:");
+            return productRepo.GetProductById(id);
+        }
+
+        public List<Product> GetProducts()
+        {
+            IRepository productRepo = new SqlRepository();
+            return productRepo.GetAllProducts();
+        }
+    }
+}
---
+++ Caching/Caching/SqlRepository.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caching
+{
+    class SqlRepository : IRepository
+    {
+        private SqlConnection con;
+        private void connection()
+        {
+            string constr = @"Data Source=TAVDESK128;Initial Catalog=ProductDetails;User ID=sa;Password=test123!@#";
+            con = new SqlConnection(constr);
+
+        }
+        public List<Product> GetAllProducts()
+        {
+            connection();
+            List<Product> productList = new List<Product>();
+            string query = "SELECT * FROM Products";
+            SqlCommand command = new SqlCommand(query, con)
+            {
+                CommandType = CommandType.Text
+            };
+            SqlDataAdapter dadapter = new SqlDataAdapter(command);
+            DataTable dtable = new DataTable();
+            con.Open();
+            dadapter.Fill(dtable);
+            con.Close();
+            foreach (DataRow datarow in dtable.Rows)
+            {
+                productList.Add(
+                    new Product
+                    {
+                        Id = Convert.ToInt32(datarow["Id"]),
+                        Name = Convert.ToString(datarow["Name"]),
+                        Price = Convert.ToInt32(datarow["Price"])
+                    }
+                );
+
+            }
+            return productList;
+        }
+
+        public Product GetProductById(int id)
+        {
+            connection();
+            List<Product> productList = new List<Product>();
+            string query = "SELECT * FROM Products where Id=" + id;
+            SqlCommand command = new SqlCommand(query, con)
+            {
+                CommandType = CommandType.Text
+            };
+            SqlDataAdapter dadapter = new SqlDataAdapter(command);
+            DataTable dtable = new DataTable();
+            con.Open();
+            dadapter.Fill(dtable);
+            con.Close();
+            foreach (DataRow datarow in dtable.Rows)
+            {
+                productList.Add(
+                    new Product
+                    {
+                        Id = Convert.ToInt32(datarow["Id"]),
+                        Name = Convert.ToString(datarow["Name"]),
+                        Price = Convert.ToInt32(datarow["Price"])
+                    }
+                );
+
+            }
+            return productList[0];
+        }
+    }
+}
